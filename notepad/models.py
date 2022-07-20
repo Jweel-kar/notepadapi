b6aa87ccc2c70d7df098ed9b91a910644434a08a
jweel <jweelkar2016@gmail.com>
2022-07-20 18:09:12 +0600

    Connect project with mysql database and create models

--- notepad/models.py
+++ notepad/models.py
@@ -1,3 +1,36 @@
+# Create your models here.
 from django.db import models
 
+
 # Create your models here.
+class Notepads(models.Model):
+  id = models.AutoField(
+    primary_key=True
+  )
+
+  title = models.TextField(
+    max_length=200,
+    null=False,
+    blank=False
+  )
+
+  text = models.TextField(
+    max_length=1000,
+    null=False,
+    blank=False
+  )
+
+  creation_date = models.DateTimeField(
+    auto_now_add=True,
+    null=False,
+    blank=False
+  )
+
+  last_updated = models.DateTimeField(
+    auto_now=True,
+    null=False,
+    blank=False
+  )
+
+  class Meta:
+    db_table = 'Notepads'
--- notepadapi/settings.py
+++ notepadapi/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Notepad apps:
+    'notepad.apps.NotepadConfig',
+
+    # Django rest framework:
     'rest_framework',
 ]
 
@@ -76,8 +81,12 @@ WSGI_APPLICATION = 'notepadapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'notepads',
+        'USER': 'jweel',
+        'PASSWORD': 'unique@59335',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
